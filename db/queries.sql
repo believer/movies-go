18cdbea93e27d16f6990db04ccf4e0eb3874037f
Rickard Natt och Dag <rickard@nattochdag.dev>
2025-01-27 21:51:47 +0100

    feat(movie): secondary sort for cast and crew by popularity
    Primary sort is still number of movies they appear in

--- db/queries.sql
+++ db/queries.sql
@@ -3,12 +3,12 @@
 -- name: cast-by-id
 SELECT
     INITCAP(mp.job::text) AS job,
-    ARRAY_AGG(p.name ORDER BY num_movies DESC) AS people_names,
-    ARRAY_AGG(p.id ORDER BY num_movies DESC) AS people_ids,
+    ARRAY_AGG(p.name ORDER BY num_movies DESC, p.popularity DESC) AS people_names,
+    ARRAY_AGG(p.id ORDER BY num_movies DESC, p.popularity DESC) AS people_ids,
     CASE mp.job
     WHEN 'cast' THEN
         ARRAY_AGG(COALESCE(mp.character, '')
-        ORDER BY num_movies DESC)
+        ORDER BY num_movies DESC, p.popularity DESC)
     ELSE
         ARRAY[]::text[]
     END AS characters
