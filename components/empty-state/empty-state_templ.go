963d00fcfee8678553e7dee89e78f022a907acbb
Rickard Natt och Dag <rickard@nattochdag.dev>
2025-09-30 20:32:05 +0200

    refactor: clean up empty state classes

--- components/empty-state/empty-state_templ.go
+++ components/empty-state/empty-state_templ.go
@@ -29,7 +29,7 @@ func EmptyState() templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<ol><li data-type=\"list-item\" class=\"list-none rounded-sm border border-dashed border-neutral-300 px-5 py-8 text-center text-neutral-500 dark:border-neutral-700 dark:text-neutral-400\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 1, "<ol><li data-type=\"list-item\" class=\"text-content-secondary list-none rounded-sm border border-dashed px-5 py-8 text-center border-border-primary\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
