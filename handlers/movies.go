3004787fcda1d1ec17a0f30665eb48b8cadb1fd9
Rickard Natt och Dag <rickard@nattochdag.dev>
2025-12-08 20:44:57 +0100

    chore: remove duplicate code for movie update

--- handlers/movies.go
+++ handlers/movies.go
@@ -234,8 +234,8 @@ func PostMovieNew(c *fiber.Ctx) error {
 	}
 
 	api := api.New(c)
-	movie, movieId, err := api.AddMovie(imdbId, data.HasWilhelmScream)
 
+	movie, movieId, err := api.AddMovie(imdbId, data.HasWilhelmScream)
 	if err != nil {
 		return err
 	}
@@ -961,30 +961,14 @@ func UpdateMovieByID(c *fiber.Ctx) error {
 	}
 
 	tmdbApi := tmdb.New(imdbId)
+	api := api.New(c)
 	movie, err := tmdbApi.Movie()
 
 	if err != nil {
 		return err
 	}
 
-	movieCast, err := tmdbApi.Credits()
-
-	if err != nil {
-		return err
-	}
-
 	tx := db.Client.MustBegin()
-
-	fmt.Println(
-		id,
-		movie.Title,
-		movie.Runtime,
-		movie.ReleaseDate,
-		movie.ImdbId,
-		movie.Overview,
-		movie.Poster,
-		movie.Tagline,
-	)
 
 	// Update movie information
 	_, err = tx.Exec(`
@@ -1021,306 +1005,11 @@ func UpdateMovieByID(c *fiber.Ctx) error {
 
 	slog.Debug("Movie updated")
 
-	// Insert languages
-	type Language struct {
-		ISO639      string `db:"iso_639_1"`
-		EnglishName string `db:"english_name"`
-		Name        string `db:"name"`
-		MovieId     int    `db:"movie_id"`
-	}
-
-	languages := make([]Language, len(movie.SpokenLanguages))
-
-	for i, l := range movie.SpokenLanguages {
-		languages[i] = Language{
-			ISO639:      l.ISO639,
-			Name:        l.Name,
-			EnglishName: l.EnglishName,
-			MovieId:     id,
-		}
-	}
-
-	if len(languages) > 0 {
-		if _, err := tx.NamedExec(
-			`INSERT INTO
-LANGUAGE (name, english_name, iso_639_1)
-    VALUES (:name, :english_name, :iso_639_1)
-ON CONFLICT
-    DO NOTHING`, languages,
-		); err != nil {
-			return err
-		}
-
-		if _, err := tx.NamedExec(
-			`INSERT INTO movie_language (movie_id, language_id)
-    VALUES (:movie_id, (
-            SELECT
-                id
-            FROM
-                LANGUAGE
-            WHERE
-                name = :name))
-ON CONFLICT
-    DO NOTHING`, languages,
-		); err != nil {
-			return err
-		}
-	}
-
-	// Insert genres
-	type Genre struct {
-		Name    string `db:"name"`
-		MovieId int    `db:"movie_id"`
-	}
-
-	genres := make([]Genre, len(movie.Genres))
-
-	for i, genre := range movie.Genres {
-		genres[i] = Genre{
-			Name:    genre.Name,
-			MovieId: id,
-		}
-	}
-
-	if len(genres) > 0 {
-		_, err = tx.NamedExec(`INSERT INTO genre (name)
-    VALUES (:name)
-ON CONFLICT (name)
-    DO NOTHING`, genres)
-
-		if err != nil {
-			return err
-		}
-
-		_, err = tx.NamedExec(`INSERT INTO movie_genre (movie_id, genre_id)
-    VALUES (:movie_id, (
-            SELECT
-                id
-            FROM
-                genre
-            WHERE
-                name = :name))
-ON CONFLICT
-    DO NOTHING`, genres)
-
-		if err != nil {
-			return err
-		}
-	}
-
-	slog.Debug("Genres updated")
-
-	for _, c := range movie.ProductionCountries {
-		tx.MustExec(`
-			INSERT INTO movie_country (movie_id, country_id)
-			    VALUES ($1, $2)
-			ON CONFLICT
-			    DO NOTHING
-    `, id, c.ID)
-	}
-
-	slog.Debug("Countries inserted")
-
-	// Production companies
-	for _, c := range movie.ProductionCompanies {
-		tx.MustExec(`
-			INSERT INTO production_company (tmdb_id, name, country)
-			    VALUES ($1, $2, $3)
-			ON CONFLICT
-			    DO NOTHING
-		`, c.ID, c.Name, c.OriginCountry)
-
-		tx.MustExec(`
-			INSERT INTO movie_company (movie_id, company_id)
-			    VALUES ($1, (
-			            SELECT
-			                id
-			            FROM
-			                production_company
-			            WHERE
-			                tmdb_id = $2))
-			ON CONFLICT
-			    DO NOTHING
-		`, id, c.ID)
-	}
-
-	slog.Debug("Production companies inserted")
-
-	var castStructs []NewPerson
-	var crewStructs []NewPerson
-
-	// Insert cast
-	for _, cast := range movieCast.Cast {
-		var pfp sql.NullString
-		var char sql.NullString
-
-		if cast.ProfilePath == nil {
-			pfp = sql.NullString{String: "", Valid: false}
-		} else {
-			pfp = sql.NullString{String: *cast.ProfilePath, Valid: true}
-		}
-
-		if cast.Character == nil {
-			char = sql.NullString{String: "", Valid: false}
-		} else {
-			char = sql.NullString{String: *cast.Character, Valid: true}
-		}
-
-		personIndex, exists := personExists(castStructs, cast.ID, "cast")
-
-		if exists {
-			castStructs[personIndex].Name = cast.Name
-			castStructs[personIndex].Popularity = cast.Popularity
-			castStructs[personIndex].Character = char
-			castStructs[personIndex].ProfilePicture = pfp
-
-			continue
-		}
-
-		castStructs = append(castStructs, NewPerson{
-			ID:             cast.ID,
-			Name:           cast.Name,
-			Popularity:     cast.Popularity,
-			Job:            sql.NullString{String: "cast", Valid: true},
-			Character:      char,
-			ProfilePicture: pfp,
-			MovieId:        id,
-		})
-	}
-
-	// Crew
-	for _, crew := range movieCast.Crew {
-		department := crew.Department
-
-		if department != "Directing" && department != "Writing" && department != "Production" && department != "Sound" && department != "Camera" && department != "Editing" {
-			continue
-		}
-
-		var pfp sql.NullString
-
-		if crew.ProfilePath == nil {
-			pfp = sql.NullString{String: "", Valid: false}
-		} else {
-			pfp = sql.NullString{String: *crew.ProfilePath, Valid: true}
-		}
-
-		if crew.Job == nil {
-			continue
-		}
-
-		job := *crew.Job
-
-		switch job {
-		case "Screenplay", "Writer", "Novel":
-			job = "writer"
-		case "Original Music Composer":
-			job = "composer"
-		case "Producer", "Associate Producer", "Executive Producer":
-			job = "producer"
-		case "Director":
-			job = "director"
-		case "Director of Photography":
-			job = "cinematographer"
-		case "Editor":
-			job = "editor"
-		default:
-			continue
-		}
-
-		jobStr := sql.NullString{String: job, Valid: true}
-		personIndex, exists := personExists(crewStructs, crew.Id, job)
-
-		if exists {
-			crewStructs[personIndex].Name = crew.Name
-			crewStructs[personIndex].Popularity = crew.Popularity
-			crewStructs[personIndex].ProfilePicture = pfp
-
-			continue
-		}
-
-		crewStructs = append(crewStructs, NewPerson{
-			ID:             crew.Id,
-			Name:           crew.Name,
-			Popularity:     crew.Popularity,
-			Job:            jobStr,
-			ProfilePicture: pfp,
-			MovieId:        id,
-		})
-	}
-
-	if len(castStructs) > 0 {
-		_, err = tx.NamedExec(`
-	INSERT INTO person (name, original_id, popularity, profile_picture)
-	    VALUES (:name, :id, :popularity, :profile_picture)
-	ON CONFLICT
-	    DO NOTHING
-	`, castStructs)
-
-		if err != nil {
-			slog.Error("Could not insert person")
-			return err
-		}
-
-		_, err = tx.NamedExec(`
-	INSERT INTO movie_person (movie_id, person_id, job, character)
-	    VALUES (:movie_id, (
-	            SELECT
-	                id
-	            FROM
-	                person
-	            WHERE
-	                original_id = :id), 'cast', :character)
-	ON CONFLICT (movie_id,
-	    person_id,
-	    job)
-	    DO UPDATE SET
-	        character = excluded.character
-	`, castStructs)
-
-		if err != nil {
-			slog.Error("Could not insert movie_person")
-			return err
-		}
-	}
-
-	slog.Debug("Cast updated")
-
-	if len(crewStructs) > 0 {
-		_, err = tx.NamedExec(`
-	INSERT INTO person (name, original_id, popularity, profile_picture)
-	    VALUES (:name, :id, :popularity, :profile_picture)
-	ON CONFLICT
-	    DO NOTHING
-	`, crewStructs)
-
-		if err != nil {
-			slog.Error("Could not insert crew")
-			return err
-		}
-
-		_, err = tx.NamedExec(`
-	INSERT INTO movie_person (movie_id, person_id, job)
-	    VALUES (:movie_id, (
-	            SELECT
-	                id
-	            FROM
-	                person
-	            WHERE
-	                original_id = :id), :job)
-	ON CONFLICT (movie_id,
-	    person_id,
-	    job)
-	    DO UPDATE SET
-	        job = excluded.job
-	`, crewStructs)
-
-		if err != nil {
-			slog.Error("Could not insert movie_person crew")
-			return err
-		}
-	}
-
-	slog.Debug("Crew updated")
+	api.AddLanguages(tx, id, movie)
+	api.AddGenres(tx, id, movie)
+	api.AddCountries(tx, id, movie)
+	api.AddProductionCompanies(tx, id, movie)
+	api.AddCast(tx, movie.ImdbId, id)
 
 	err = tx.Commit()
 
