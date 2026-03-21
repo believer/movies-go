778a598c70f7d2395503b076d78810ebfc0b891b
Rickard Natt och Dag <rickard@nattochdag.dev>
2026-03-21 06:28:16 +0100

    chore: change title of awards stats

--- views/stats_templ.go
+++ views/stats_templ.go
@@ -740,7 +740,7 @@ func Stats(props StatsProps) templ.Component {
 				}
 				return nil
 			})
-			templ_7745c5c3_Err = section.Section(section.Props{Title: "Academy Awards"}).Render(templ.WithChildren(ctx, templ_7745c5c3_Var26), templ_7745c5c3_Buffer)
+			templ_7745c5c3_Err = section.Section(section.Props{Title: "Awards"}).Render(templ.WithChildren(ctx, templ_7745c5c3_Var26), templ_7745c5c3_Buffer)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
